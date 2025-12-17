9fa42b16fece1b6a85f8b1a78b1ee0c35cc42dde
amit-pant-acc <amit.b.pant@accenture.com>
2025-12-17 12:49:47 +0000

    setup and test endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-^v=3680cd(#9+b65mtza6mjz++uk30sbk33*qp1ba6+kh9y)ff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,17 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+import os
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f"https://{codespace_name}-8000.app.github.dev/"
+    else:
+        base_url = request.build_absolute_uri('/')
+    api_url = lambda comp: f"{base_url}api/{comp}/"
     return Response({
-        'users': request.build_absolute_uri('api/users/'),
-        'teams': request.build_absolute_uri('api/teams/'),
-        'activities': request.build_absolute_uri('api/activities/'),
-        'workouts': request.build_absolute_uri('api/workouts/'),
-        'leaderboard': request.build_absolute_uri('api/leaderboard/'),
+        'users': api_url('users'),
+        'teams': api_url('teams'),
+        'activities': api_url('activities'),
+        'workouts': api_url('workouts'),
+        'leaderboard': api_url('leaderboard'),
     })
 
 urlpatterns = [
